d8edd8785995ecf623f0b879dc49d9f26fd9558e
sachin320 <sachinvermavs786@gmail.com>
2021-11-08 11:11:15 +0530

    header component added, routes added for shop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 function App() {
   return (
     <div>
       <Router>
+      <Header />
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
           <Route path="/shop" component={ShopPage} />
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import './header.style.css'
+
+import { Link } from "react-router-dom";
+import NavLogo from '../../images/logo.png'
+const Header = () =>{
+    return(
+        <div className="header">
+            <Link to="/"><img className="header-img" src={NavLogo} alt="header-img" /></Link>
+            <div className="options">
+            <Link className="option" to="/shop">Shop</Link>
+            <Link className="option" to="/contact">Contact</Link>
+            </div>
+        </div>
+    )
+}
+
+
+export default Header;
