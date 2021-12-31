34f9840221a2e125d9fbdbe48e9c4a6afac67e0b
sachin320 <sachinvermavs786@gmail.com>
2021-12-31 16:14:04 +0530

    minor updates

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import './App.css';
-import React, { useEffect, useState} from 'react';
+import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import signin from './pages/signin/signin.component';
-import { auth, userDocData } from "./firebase/firebase.utils";
 function App() {
-  const [authUser, setUser] = useState(null);
-    useEffect(() =>{
-          auth.onAuthStateChanged(async user =>{
-            if(user) {
-              const docRef = await userDocData(user);
-              docRef.onSnapshot(snapshot => {
-                setUser({
-                  id : snapshot.id,
-                  ...snapshot.data()
-                })
-              })
-            }else{
-              setUser(null)
-            } 
-        })
-    }, [])
   return (
     <div>
       <Router>
-      <Header currentUser={authUser} />
+      <Header />
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
-          <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={signin} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={signin} />
+          <Redirect from="*" to="/" />
         </Switch>
       </Router>
     </div>
