ee33a7ec797dccc509eccc5fd892df653577ba3c
sachin320 <sachinvermavs786@gmail.com>
2022-01-01 21:46:28 +0530

    Cart icon updated and styling updated

--- src/components/cart-icon/cart.component.jsx
+++ src/components/cart-icon/cart.component.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 
 import { toogleCart } from "../../redux/action/cart";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {ReactComponent as ShopingIcon} from "../../images/shopping-bag.svg"
 import './cart.style.css'
 
 const CartIcon = () =>{
     const dispatch = useDispatch();
+    const data = useSelector(state => state.cartReducer.cartItems)
     return(
         <div className="cart-icon" onClick={() => {
             dispatch(toogleCart())
         }  }>
             <ShopingIcon className="shopping-icon" />
-            <span className="item-count"> 0 </span>
+            <span className="item-count"> {data.length} </span>
         </div>
     )
 }
--- src/components/cart-items/cart-item.component.jsx
+++ src/components/cart-items/cart-item.component.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 
-import './cart-item.style.css'
-const CartItem = ({item}) =>{
-    const { imageUrl, price, quantity } = item
-    return(
-        <div className="cart-item">
-            <img src={imageUrl} alt="CartImage" />
-            <span className="price">
-                {quantity} X ${price}
-            </span>
-        </div>
-    )
-}
+import "./cart-item.style.css";
+const CartItem = ({ item }) => {
+  const { imageUrl, price, quantity, name } = item;
+  return (
+    <div className="cart-item">
+      <div>
+        <img src={imageUrl} alt="CartImage" />
+      </div>
+      <div>
+          <p>{name}</p>
+        <span className="price">
+          {quantity} X ${price}
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default CartItem
+export default CartItem;
